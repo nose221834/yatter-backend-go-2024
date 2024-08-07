5d18a8e69dd0a612fd2ad39441011d8f9003da0e
nose221834 <iatyy01151821@gmail.com>
2024-08-07 12:36:16 +0900

    URLからusernameを取得するように修正

--- app/handler/accounts/find.go
+++ app/handler/accounts/find.go
@@ -2,36 +2,36 @@ package accounts
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type FindRequest struct {
 	Username string
 }
 
-func (h *handler) Find(w http.ResponseWriter, r *http.Request) {
-	var req FindRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w,err.Error(),http.StatusBadRequest)
+func (h *handler) FindByUsername(w http.ResponseWriter, r *http.Request) {
+
+	// URLパラメータからusernameを取得
+	username := chi.URLParam(r, "username")
+	if username == "" {
+		http.Error(w, "username is required", http.StatusBadRequest)
 		return
 	}
+	fmt.Println("Value of x:", username)
 
 	ctx := r.Context()
 
-	dto ,err := h.accountUsecase.Find(ctx,req.Username)
-	if err := nil {
-		http.Error(w,err.Error(),http.StatusInternalServerError)
+	dto, err := h.accountUsecase.FindByUsername(ctx, username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(dto.Account); err != nil {
-		http.Error(w,err.Error(),http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
-
-
-
-
-
 }
