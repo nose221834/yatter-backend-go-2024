744f5f07796d443e6f52591d82f4bb780038f41b
nose221834 <iatyy01151821@gmail.com>
2024-08-08 11:56:43 +0900

    add status table

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `account` (
+CREATE TABLE IF NOT EXISTS `account` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL UNIQUE,
   `password_hash` varchar(255) NOT NULL,
@@ -8,4 +8,13 @@ CREATE TABLE `account` (
   `note` text,
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `status` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `AccountID` INT NOT NULL,
+  `URL` VARCHAR(255),
+  `Content` TEXT NOT NULL,
+  `CreatedAt` DATETIME,
+  PRIMARY KEY (`ID`)
 );
