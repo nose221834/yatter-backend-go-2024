ca01ffa2d9fe9244d709841cc684da6d18472e29
nose221834 <iatyy01151821@gmail.com>
2024-08-09 13:19:36 +0900

    accountにエラーハンドリングを追加

--- app/handler/accounts/find.go
+++ app/handler/accounts/find.go
@@ -26,6 +26,10 @@ func (h *handler) FindByUsername(w http.ResponseWriter, r *http.Request) {
 
 	dto, err := h.accountUsecase.FindByUsername(ctx, username)
 	if err != nil {
+		if dto == nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
